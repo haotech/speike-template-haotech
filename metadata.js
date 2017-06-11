626133d428a71848d15122bc75778b71f321f15f
berwin <berwin1995@qq.com>
2017-06-11 19:43:39 +0800

    update

---
+++ metadata.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "message": "Project description",
+      "default": "A Vue.js project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
+  }
+};
